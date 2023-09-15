962c5add64cc7f1f6d414d31a9912c74a046599a
MannySotoRuiz <sotoruiz.emmanuel@gmail.com>
2023-09-14 23:30:12 -0400

    created recent posts section

---
+++ src/components/card/Card.jsx
@@ -0,0 +1,35 @@
+import Image from "next/image";
+import styles from "./card.module.css";
+import Link from "next/link";
+
+const Card = () => {
+  return (
+    <div className={styles.container}>
+      <div className={styles.imageContainer}>
+        <Image src="/p1.jpeg" alt="" fill className={styles.image} />
+      </div>
+      <div className={styles.textContainer}>
+        <div className={styles.detail}>
+          <span className={styles.date}>11.02.2023 - </span>
+          <span className={styles.category}>Culture</span>
+        </div>
+        <Link href="/">
+          <h1 className={styles.title}>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit.
+          </h1>
+        </Link>
+        <p className={styles.desc}>
+          Lorem Ipsum is simply dummy text of the printing and typesetting
+          industry. Lorem Ipsum has been the industry's standard dummy text ever
+          since the 1500s, when an unknown printer took a galley of type and
+          scrambled it to make a type specimen book.
+        </p>
+        <Link href="/" className={styles.link}>
+          Read More
+        </Link>
+      </div>
+    </div>
+  );
+};
+
+export default Card;
