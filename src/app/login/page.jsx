2687b89ea24ea8041cbcc2e37d2a51dc6421eab8
MannySotoRuiz <sotoruiz.emmanuel@gmail.com>
2023-09-16 01:26:33 -0400

    added nextAuth and implemented google login

--- src/app/login/page.jsx
+++ src/app/login/page.jsx
@@ -1,10 +1,27 @@
+"use client";
+import { signIn, useSession } from "next-auth/react";
 import styles from "./loginPage.module.css";
+import { useRouter } from "next/navigation";
 
-const page = () => {
+const LoginPage = () => {
+  const { status } = useSession();
+
+  const router = useRouter();
+
+  if (status === "loading") {
+    return <div className={styles.loading}>Loading...</div>;
+  }
+
+  if (status === "authenticated") {
+    router.push("/");
+  }
+
   return (
     <div className={styles.container}>
       <div className={styles.wrapper}>
-        <div className={styles.socialButton}>Sign in with Google</div>
+        <div className={styles.socialButton} onClick={() => signIn("google")}>
+          Sign in with Google
+        </div>
         <div className={styles.socialButton}>Sign in with Github</div>
         <div className={styles.socialButton}>Sign in with Facebook</div>
       </div>
@@ -12,4 +29,4 @@ const page = () => {
   );
 };
 
-export default page;
+export default LoginPage;
--- src/components/authLinks/AuthLinks.jsx
+++ src/components/authLinks/AuthLinks.jsx
@@ -3,6 +3,7 @@
 import Link from "next/link";
 import styles from "./authLinks.module.css";
 import { useState } from "react";
+import { signOut } from "next-auth/react";
 
 const AuthLinks = () => {
   const [open, setOpen] = useState(false);
@@ -18,7 +19,9 @@ const AuthLinks = () => {
           <Link href="/write" className={styles.link}>
             Write
           </Link>
-          <span className={styles.link}>Logout</span>
+          <span className={styles.link} onClick={signOut}>
+            Logout
+          </span>
         </>
       )}
       <div className={styles.burger} onClick={() => setOpen(!open)}>
@@ -36,7 +39,9 @@ const AuthLinks = () => {
           ) : (
             <>
               <Link href="/write">Write</Link>
-              <span className={styles.link}>Logout</span>
+              <span className={styles.link} onClick={signOut}>
+                Logout
+              </span>
             </>
           )}
         </div>
