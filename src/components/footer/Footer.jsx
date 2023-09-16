87e2c5210e479a33aba60d6ebb70bbaab8cd6e93
MannySotoRuiz <sotoruiz.emmanuel@gmail.com>
2023-09-15 20:27:13 -0400

    finished footer component

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,8 +1,54 @@
 import React from "react";
 import styles from "./footer.module.css";
+import Image from "next/image";
+import Link from "next/link";
 
 const Footer = () => {
-  return <div className={styles.container}>Footer</div>;
+  return (
+    <div className={styles.container}>
+      <div className={styles.info}>
+        <div className={styles.logo}>
+          <Image src="/logo.png" alt="Manny Blog" width={50} height={50} />
+          <h1 className={styles.logoText}>MannyBlog</h1>
+        </div>
+        <p className={styles.desc}>
+          Lorem Ipsum is simply dummy text of the printing and typesetting
+          industry. Lorem Ipsum has been the industry's standard dummy text ever
+          since the 1500s, when an unknown printer took a galley of type and
+          scrambled it to make a type specimen book.
+        </p>
+        <div className={styles.icons}>
+          <Image src="/facebook.png" alt="facebook" width={18} height={18} />
+          <Image src="/instagram.png" alt="facebook" width={18} height={18} />
+          <Image src="/tiktok.png" alt="facebook" width={18} height={18} />
+          <Image src="/youtube.png" alt="facebook" width={18} height={18} />
+        </div>
+      </div>
+      <div className={styles.links}>
+        <div className={styles.list}>
+          <span className={styles.listTitle}>Links</span>
+          <Link href="/">Homepage</Link>
+          <Link href="/">Blog</Link>
+          <Link href="/">About</Link>
+          <Link href="/">Contact</Link>
+        </div>
+        <div className={styles.list}>
+          <span className={styles.listTitle}>Tags</span>
+          <Link href="/">Style</Link>
+          <Link href="/">Fashion</Link>
+          <Link href="/">Coding</Link>
+          <Link href="/">Travel</Link>
+        </div>
+        <div className={styles.list}>
+          <span className={styles.listTitle}>Social</span>
+          <Link href="/">Facebook</Link>
+          <Link href="/">Instagram</Link>
+          <Link href="/">Tiktok</Link>
+          <Link href="/">YouTube</Link>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Footer;
